85e5044bf8cc43dd5795e465b16cda3914cb96a0
Taisuke Fukuno <fukuno@jig.jp>
2021-10-31 07:03:20 +0900

    add improved fib.js

--- example/fib.js
+++ example/fib.js
@@ -1,1 +1,9 @@
-export const fib = (n) => n <= 1 ? n : fib(n - 1) + fib(n - 2);
+const cache = [];
+
+export const fib = (n) => {
+  const c = cache[n];
+  if (c != undefined) {
+    return c;
+  }
+  return cache[n] = n <= 1 ? n : fib(n - 1) + fib(n - 2);
+};
